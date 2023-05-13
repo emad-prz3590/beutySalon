5b86b88cd55bb386e4bf0486ea70c0f96ffec06b
emad <emadpz1997@gmail.com>
2023-05-13 16:57:31 +0330

    chnage for

--- beauty2/about/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'about'
-
-urlpatterns= [
-    path('about/', about ,name='about'),
-]
--- beauty2/blog/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class ArticleSearchForm(forms.Form):
-    search = forms.CharField(label='جستجوی مقالات', max_length=100)
